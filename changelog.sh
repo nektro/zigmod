118f1110067a4ab5cc3583ee0ab030f27f684300
Meghan <hello@nektro.net>
2020-11-09 14:52:56 -0800

    Create changelog.sh

---
+++ changelog.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -e
+
+read-log() {
+    git log --format=format:"%h%n%H%n%an%n%s%n%d%n"
+}
+
+hash_abrev=''
+hash=''
+author=''
+title=''
+
+c=0
+t=0
+read-log |
+while IFS= read -r lineVAR; do
+    if [[ "$c" == '0' ]]; then
+        hash_abrev="$lineVAR"
+    fi
+    if [[ "$c" == '1' ]]; then
+        hash="$lineVAR"
+    fi
+    if [[ "$c" == '2' ]]; then
+        author="$lineVAR"
+    fi
+    if [[ "$c" == '3' ]]; then
+        title="$lineVAR"
+    fi
+    if [[ "$c" == '4' ]]; then
+        if [ ! -z "$lineVAR" ]; then
+            t=$(($t+1))
+        fi
+        if [[ "$t" == '2' ]]; then
+            break
+        fi
+        echo "<li><a href='https://github.com/nektro/$CIRCLE_PROJECT_REPONAME/commit/$hash'>$hash_abrev</a> $title ($author)</li>"
+    fi
+    c=$(($c+1))
+    #
+    if [[ "$c" == '6' ]]; then
+        c=0
+    fi
+done
