865feab433b6368486332d7b69969737f7230904
Meghan <hello@nektro.net>
2020-11-09 11:55:29 -0800

    add main

--- main.go
+++ main.go
@@ -1,3 +1,26 @@
+package main
+
+import (
+	"github.com/nektro/go-util/util"
+	"github.com/spf13/cobra"
+)
+
+// Version takes actual string in from build_all.sh
+const Version = "vMASTER"
+
+func main() {
+	err := rootCmd.Execute()
+	util.DieOnError(err)
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "zigmod",
+	Short: "Zigmod is a package manager for Zig.",
+	Long:  `Get documentation, binaries, and more from https://github.com/nektro/zigmod.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+	},
+}
 
 func assert(x bool, msg string) {
 	util.DieOnError(util.Assert(x, msg))
