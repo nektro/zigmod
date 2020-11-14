acc5fa4deed75353b3d36dbf81e5366ceeefba88
Meghan <hello@nektro.net>
2020-11-14 02:11:43 -0800

    zig: add util/modfile

---
+++ src/util/modfile.zig
@@ -0,0 +1,55 @@
+const std = @import("std");
+
+const u = @import("index.zig");
+const yaml = @import("./yaml.zig");
+
+//
+//
+
+const b = 1;
+const kb = b * 1024;
+const mb = kb * 1024;
+
+pub const ModFile = struct {
+    const Self = @This();
+
+    alloc: *std.mem.Allocator,
+    name: []const u8,
+    main: []const u8,
+    deps: []u.Dep,
+
+    pub fn init(alloc: *std.mem.Allocator, fpath: []const u8) !Self {
+        //
+        const mpath = try std.fs.realpathAlloc(alloc, fpath);
+        const file = try std.fs.openFileAbsolute(mpath, .{});
+        defer file.close();
+        const input = try file.reader().readAllAlloc(alloc, mb);
+        const doc = try yaml.parse(alloc, input);
+
+        const name = doc.mapping.get("name").?.string;
+        const main = doc.mapping.get("main").?.string;
+
+        const dep_list = &std.ArrayList(u.Dep).init(alloc);
+        if (doc.mapping.get("dependencies")) |dep_seq| {
+            if (dep_seq == .sequence) {
+                for (dep_seq.sequence) |item| {
+                    const dtype = item.mapping.get("type").?.string;
+                    const path = item.mapping.get("path").?.string;
+                    const dep_type = std.meta.stringToEnum(u.DepType, dtype).?;
+
+                    try dep_list.append(u.Dep{
+                        .type = dep_type,
+                        .path = path,
+                    });
+                }
+            }
+        }
+
+        return Self{
+            .alloc = alloc,
+            .name = name,
+            .main = main,
+            .deps = dep_list.items,
+        };
+    }
+};
