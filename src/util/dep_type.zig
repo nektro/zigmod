218eb5f856f217e958788fcde9ac5d52d7cc75e7
Meghan <hello@nektro.net>
2020-11-14 02:10:40 -0800

    zig: add util/dep_type

---
+++ src/util/dep_type.zig
@@ -0,0 +1,10 @@
+const std = @import("std");
+
+const u = @import("index.zig");
+
+//
+//
+
+pub const DepType = enum {
+    git,        // https://git-scm.com/
+};
--- src/util/index.zig
+++ src/util/index.zig
@@ -1,3 +1,4 @@
 usingnamespace @import("./ascii.zig");
 usingnamespace @import("./ansi.zig");
 usingnamespace @import("./funcs.zig");
+usingnamespace @import("./dep_type.zig");
