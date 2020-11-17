313f98c4a8495cb252cd3bd55531c691ccd13e32
Meghan <hello@nektro.net>
2020-11-16 23:12:14 -0800

    make_release- remove verbose opt from go get

--- make_release.sh
+++ make_release.sh
@@ -5,7 +5,7 @@ set -e
 date=$(date +'%Y.%m.%d')
 version=${CIRCLE_BUILD_NUM-$date}
 tag=v$version-$(git log --format=%h -1)
-go get -v github.com/tcnksm/ghr
+go get github.com/tcnksm/ghr
 $GOPATH/bin/ghr \
     -t ${GITHUB_TOKEN} \
     -u ${CIRCLE_PROJECT_USERNAME} \
