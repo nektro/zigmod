c5ef44d0423dedd88992dae7acc152fe6f3de7b1
Meghan <hello@nektro.net>
2020-11-20 13:35:05 -0800

    add missing git_version_type

--- src/util/dep_type.zig
+++ src/util/dep_type.zig
@@ -5,3 +5,9 @@ pub const DepType = enum {
     git,        // https://git-scm.com/
     hg,         // https://www.mercurial-scm.org/
 };
+
+pub const GitVersionType = enum {
+    branch,
+    tag,
+    commit,
+};
