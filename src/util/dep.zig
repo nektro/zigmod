78a35f3bc98a380f06a374f34f23af903315cafa
Meghan <hello@nektro.net>
2020-12-05 10:05:12 -0800

    util/dep- trim http and https from path

--- src/util/dep.zig
+++ src/util/dep.zig
@@ -24,7 +24,7 @@ pub const Dep = struct {
 
     pub fn clean_path(self: Dep) ![]const u8 {
         var p = self.path;
-        p = u.trim_prefix(p, "https://");
+        p = u.trim_prefix(p, "http://");
         p = u.trim_prefix(p, "https://");
         p = u.trim_suffix(u8, p, ".git");
         p = try std.fmt.allocPrint(gpa, "{}{}{}", .{@tagName(self.type), "/", p});
