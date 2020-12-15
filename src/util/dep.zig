e200cf1b5505491274cbf12cdeb0478bf13f2370
Meghan <hello@nektro.net>
2020-12-15 04:44:28 -0800

    dep- remove git protocol prefex from paths

--- src/util/dep.zig
+++ src/util/dep.zig
@@ -26,6 +26,7 @@ pub const Dep = struct {
         var p = self.path;
         p = u.trim_prefix(p, "http://");
         p = u.trim_prefix(p, "https://");
+        p = u.trim_prefix(p, "git://");
         p = u.trim_suffix(u8, p, ".git");
         p = try std.fmt.allocPrint(gpa, "{}{}{}", .{@tagName(self.type), "/", p});
         return p;
