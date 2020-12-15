ef9dd1621b45f3280be48d7519e2e5a5626b1796
Meghan <hello@nektro.net>
2020-12-15 02:26:44 -0800

    build_all_zig.sh: use dot between build and git hash
    hyphen is used to separate major fields and the git hash is part of the version field

--- build_all_zig.sh
+++ build_all_zig.sh
@@ -4,7 +4,7 @@ set -e
 
 date=$(date +'%Y.%m.%d')
 version=${CIRCLE_BUILD_NUM-$date}
-tag=v$version-$(git log --format=%h -1)
+tag=v$version.$(git log --format=%h -1)
 
 for item in $(zig targets | jq --raw-output '.libc[]' | grep gnu$ | grep x86_64)
 do
