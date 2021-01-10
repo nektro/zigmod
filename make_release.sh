9ae8d3807b792d1651d41fa14b42a2e7ecb786f9
Meghan <hello@nektro.net>
2021-01-09 16:59:29 -0800

    remove git hash from tag name

--- make_release.sh
+++ make_release.sh
@@ -4,7 +4,7 @@ set -e
 
 date=$(date +'%Y.%m.%d')
 version=${CIRCLE_BUILD_NUM-$date}
-tag=v$version-$(git log --format=%h -1)
+tag=v$version
 go get github.com/tcnksm/ghr
 $GOPATH/bin/ghr \
     -t ${GITHUB_TOKEN} \
