31e2be3d40050b7dd24de7d96ea0c6463d111db6
Meghan Denny <hello@nektro.net>
2021-01-29 15:07:25 -0800

    main: export commands var

--- src/main.zig
+++ src/main.zig
@@ -6,7 +6,7 @@ const u = @import("./util/index.zig");
 //
 //
 
-const commands = struct {
+pub const commands = struct {
     const init = @import("./cmd_init.zig");
     // const add = @import("./cmd_add.zig");
     const fetch = @import("./cmd_fetch.zig");
