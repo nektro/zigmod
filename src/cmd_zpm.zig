3df411d44f33f2bc6cb1ca28c3e517b249fc92fb
Meghan Denny <hello@nektro.net>
2021-01-31 12:04:01 -0800

    add zpm command

---
+++ src/cmd_zpm.zig
@@ -0,0 +1,32 @@
+const std = @import("std");
+const gpa = std.heap.c_allocator;
+
+const u = @import("./util/index.zig");
+
+//
+//
+
+const commands = struct {
+};
+
+pub fn execute(args: [][]u8) !void {
+    if (args.len == 0) {
+        std.debug.warn("{s}\n", .{
+            \\This is a subcommand for use with https://github.com/zigtools/zpm-server instances but has no default behavior on its own aside from showing you this nice help text.
+            \\
+            \\The default remote is https://zpm.random-projects.net/.
+            \\
+            \\The subcommands available are:
+        });
+        return;
+    }
+
+    inline for (std.meta.declarations(commands)) |decl| {
+        if (std.mem.eql(u8, args[0], decl.name)) {
+            const cmd = @field(commands, decl.name);
+            try cmd.execute(args[1..]);
+            return;
+        }
+    }
+    std.debug.panic("error: unknown command \"{s}\" for \"zigmod zpm\"", .{args[0]});
+}
