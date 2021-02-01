c39d45ad74461ada6e4d58ab4f7c251dd38320dd
Meghan Denny <hello@nektro.net>
2021-02-01 08:57:48 -0800

    download_zig- fix fetch location for master builds

--- download_zig.sh
+++ download_zig.sh
@@ -11,6 +11,12 @@ dir="zig-$os-$arch-$version"
 file="$dir.tar.xz"
 
 cd /
-wget https://ziglang.org/download/$version/$file
 tar -xf $file
 ln -s /$dir/zig /usr/local/bin
+
+if [[ $1 == *"dev"* ]]; then
+    wget https://ziglang.org/builds/$file
+else
+    wget https://ziglang.org/download/$version/$file
+fi
+
