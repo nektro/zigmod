51e232718355b5f0d3be9732c7e545c4335adc07
Meghan Denny <hello@nektro.net>
2021-05-06 02:09:02 -0700

    cmd/aq/showjson: missed `zig fmt` run

--- src/cmd/aquila/showjson.zig
+++ src/cmd/aquila/showjson.zig
@@ -10,7 +10,7 @@ const aq = @import("./../aq.zig");
 pub fn execute(args: [][]u8) !void {
     const url = try std.mem.join(gpa, "/", &.{ aq.server_root, args[0] });
     const val = try aq.server_fetch(url);
-    
+
     const out = std.io.getStdOut().writer();
     try out.print("{}\n", .{val});
 }
