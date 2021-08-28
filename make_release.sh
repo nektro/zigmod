618dfb843dd2787a7382f816d9c2768cfff7872a
Meghan Denny <hello@nektro.net>
2021-08-28 12:52:24 -0700

    make_release.sh- switch to zig ghr

--- make_release.sh
+++ make_release.sh
@@ -5,8 +5,8 @@ set -e
 date=$(date +'%Y.%m.%d')
 version=${CIRCLE_BUILD_NUM-$date}
 tag=v$version
-go get github.com/tcnksm/ghr
-$GOPATH/bin/ghr \
+zigmod aq install 1/nektro/ghr
+~/.zigmod/bin/ghr \
     -t ${GITHUB_TOKEN} \
     -u ${CIRCLE_PROJECT_USERNAME} \
     -r ${CIRCLE_PROJECT_REPONAME} \
