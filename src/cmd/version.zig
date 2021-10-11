4af6aa9409cc8e61c11d270008e9238d137b067b
Meghan Denny <hello@nektro.net>
2021-10-11 11:59:32 -0700

    cmd/verison- catch git_rev_HEAD and fallback, fixes #35

--- src/cmd/version.zig
+++ src/cmd/version.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const string = []const u8;
 const gpa = std.heap.c_allocator;
 const builtin = @import("builtin");
 
@@ -14,19 +15,17 @@ pub fn execute(args: [][]u8) !void {
     const build_options = if (@hasDecl(root, "build_options")) root.build_options else struct {};
     const version = if (@hasDecl(build_options, "version")) build_options.version else "unknown";
 
+    var gitversion = u.git_rev_HEAD(gpa, std.fs.cwd()) catch "";
+    gitversion = if (gitversion.len > 0) gitversion[0..7] else gitversion;
+
     const stdout = std.io.getStdOut();
     const w = stdout.writer();
 
     try w.writeAll("zigmod");
 
     try w.print(" {s}", .{version});
-    if (std.mem.eql(u8, version, "dev")) {
-        try w.print(
-            "-{s}",
-            .{
-                (try u.git_rev_HEAD(gpa, std.fs.cwd()))[0..7],
-            },
-        );
+    if (std.mem.eql(u8, version, "dev") and gitversion.len > 0) {
+        try w.print("-{s}", .{gitversion});
     }
 
     try w.print(" {s}", .{@tagName(builtin.os.tag)});
