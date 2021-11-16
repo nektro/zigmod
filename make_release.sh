bea44ffb826b52ff37c290a004536b85157887ba
Meghan Denny <hello@nektro.net>
2021-11-15 23:12:23 -0800

    make_release- use a ghr binary to break the dep loop

--- make_release.sh
+++ make_release.sh
@@ -5,10 +5,11 @@ set -e
 date=$(date +'%Y.%m.%d')
 version=${CIRCLE_BUILD_NUM-$date}
 tag=v$version
-./zig-out/bin/zigmod aq install 1/nektro/ghr
-echo
-rm /artifacts/zigmod
-~/.zigmod/bin/ghr \
+
+# ghr uses Zigmod to build itself, so we need to use the binary to build Zigmod
+curl -s https://api.github.com/repos/nektro/ghr-zig/releases | jq -r '.[0].assets[].browser_download_url' | grep $(uname -m) | grep linux | wget -i -
+chmod +x ./ghr-linux-x86_64
+./ghr-linux-x86_64 \
     -t ${GITHUB_TOKEN} \
     -u ${CIRCLE_PROJECT_USERNAME} \
     -r ${CIRCLE_PROJECT_REPONAME} \
