df9cbb33a9e539c9075751edefb51453fde9d6fd
Meghan Denny <hello@nektro.net>
2021-12-02 02:19:23 -0800

    common/get_module_from_dep- was passing the wrong value to Module.from

--- src/common.zig
+++ src/common.zig
@@ -235,7 +235,7 @@ pub fn get_module_from_dep(d: *zigmod.Dep, cachepath: string, options: *CollectO
             var dd = try collect_deps(cachepath, moddir, options) catch |e| switch (e) {
                 error.FileNotFound => {
                     if (d.main.len > 0 or d.c_include_dirs.len > 0 or d.c_source_files.len > 0 or d.keep) {
-                        var mod_from = try zigmod.Module.from(options.alloc, d.*, cachepath, options);
+                        var mod_from = try zigmod.Module.from(options.alloc, d.*, modpath, options);
                         if (d.type != .local) mod_from.clean_path = u.trim_prefix(modpath, cachepath)[1..];
                         if (mod_from.is_for_this()) return mod_from;
                         return null;
