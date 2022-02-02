830f3b6aad52bfbfdd9f489ed1bd54a866829670
Meghan Denny <hello@nektro.net>
2022-02-02 03:24:12 -0800

    add aarch64-windows as a build target

--- build_all_zig.sh
+++ build_all_zig.sh
@@ -9,10 +9,11 @@ tag=v$version.$(git log --format=%h -1)
 targets="
 aarch64-linux-musl
 aarch64-macos-gnu
+aarch64-windows-gnu
 i386-linux-musl
 x86_64-linux-musl
-x86_64-windows-gnu
 x86_64-macos-gnu
+x86_64-windows-gnu
 "
 
 for item in $targets
