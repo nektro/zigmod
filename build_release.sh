996d986c996233f1f02f67b04365c71ce097c148
MisileLaboratory <misileminecord@gmail.com>
2022-10-01 21:48:53 +0900

    maybe we can just use $1

--- build_release.sh
+++ build_release.sh
@@ -5,12 +5,10 @@ set -e
 tag="r$(./release_num.sh)"
 rev=$(git log --format=%h -1)
 
-target=$1
-
 if [[ "$1" == "" ]]
 then
   exit 2
 fi
 
 echo "$tag.$rev $target"
-$(which time) zig build -Dtarget=$target -Duse-full-name -Dtag=$tag --prefix .
+$(which time) zig build -Dtarget="$1" -Duse-full-name -Dtag=$tag --prefix .
