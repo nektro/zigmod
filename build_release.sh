fc6ffd50aa2184ca983d248d27ed79a21ab5e037
Meghan Denny <hello@nektro.net>
2023-08-14 21:08:32 -0700

    build_release.sh: solve TODO

--- build_release.sh
+++ build_release.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-set -e
+set -eu
 
 tag="r$(./release_num.sh)"
 rev=$(git log --format=%h -1)
 
 target=$1
-
-# TODO error if $target is empty
 
 echo "$tag.$rev $target"
 $(which time) zig build -Dtarget=$target -Duse-full-name -Dtag=$tag --prefix .
