3caa1188c9e0d926b51908e42905162b4551668a
Meghan Denny <hello@nektro.net>
2024-06-02 16:32:59 -0700

    nektro/ghr-zig renamed to nektro/ghr

--- make_release.sh
+++ make_release.sh
@@ -9,7 +9,7 @@ PROJECT_USERNAME=$(echo $GITHUB_REPOSITORY | cut -d'/' -f1)
 PROJECT_REPONAME=$(echo $GITHUB_REPOSITORY | cut -d'/' -f2)
 
 # ghr uses Zigmod to build itself, so we need to use the binary to build Zigmod
-curl -s https://api.github.com/repos/nektro/ghr-zig/releases | jq -r '.[0].assets[].browser_download_url' | grep $(uname -m) | grep linux | wget -i -
+curl -s https://api.github.com/repos/nektro/ghr/releases | jq -r '.[0].assets[].browser_download_url' | grep $(uname -m) | grep linux | wget -i -
 chmod +x ./ghr-linux-x86_64
 ./ghr-linux-x86_64 \
     -t ${GITHUB_TOKEN} \
