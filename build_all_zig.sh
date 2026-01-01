9649704a3c29d0135bdbf6c67a74bfa229dc4951
Meghan Denny <hello@nektro.net>
2026-01-01 01:20:16 -0800

    ci: space these calls out

--- build_all_zig.sh
+++ build_all_zig.sh
@@ -8,13 +8,19 @@ targets="
 x86_64-linux-musl
 x86_64-macos-none
 x86_64-windows-gnu
+
 aarch64-linux-musl
 aarch64-macos-none
 aarch64-windows-gnu
+
 riscv64-linux-musl
+
 powerpc64le-linux-musl
+
 mips64el-linux-muslabi64
+
 s390x-linux-musl
+
 loongarch64-linux-musl
 "
 
