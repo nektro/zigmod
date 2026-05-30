nektro/zigmod

commit 8e666c9d94f08ea4ef992081e7e6b0637cf80a18

ci: build release binaries after test

.github/workflows/push.yml
@@ -27,21 +27,6 @@ jobs:
       - run: zig build -j1
       - run: echo "$PWD/zig-out/bin" >> $GITHUB_PATH
 
-      # Build All
-      - run: ./build_release.sh x86_64-linux-musl
-      - run: ./build_release.sh x86_64-macos-none
-
-      - run: ./build_release.sh aarch64-linux-musl
-      - run: ./build_release.sh aarch64-macos-none
-
-      - run: ./build_release.sh riscv64-linux-musl
-
-      - run: ./build_release.sh powerpc64le-linux-musl
-
-      - run: ./build_release.sh mips64el-linux-muslabi64
-
-      - run: ./build_release.sh s390x-linux-musl
-
       # Test
       - run: zigmod version
       - run: zigmod ci
@@ -59,6 +44,21 @@ jobs:
       - run: ./test_repo.sh https://github.com/nektro/zigmod-test-systemlib-dep
       - run: ./test_repo.sh https://github.com/nektro/zigmod-test-local-dep
       - run: ./test_repo.sh https://github.com/nektro/zigmod-test-c-code
+
+      # Build All
+      - run: ./build_release.sh x86_64-linux-musl
+      - run: ./build_release.sh x86_64-macos-none
+
+      - run: ./build_release.sh aarch64-linux-musl
+      - run: ./build_release.sh aarch64-macos-none
+
+      - run: ./build_release.sh riscv64-linux-musl
+
+      - run: ./build_release.sh powerpc64le-linux-musl
+
+      - run: ./build_release.sh mips64el-linux-muslabi64
+
+      - run: ./build_release.sh s390x-linux-musl
 
       # Github Release
       - run: ./changelog.sh

commit aa9bdacb3fa1fa747b3dcf440c0c3994ed25f992

ci: update to 0.14.1
broke after 8e666c9d94f08ea4ef992081e7e6b0637cf80a18

.github/workflows/push.yml
@@ -20,7 +20,7 @@ jobs:
         uses: mlugg/setup-zig@v2
         with:
           mirror: "http://mirrors.nektro.net/s3cgi"
-          version: "0.14.0"
+          version: "0.14.1"
 
       - run: zig version
       - run: zig env

deps.zig
@@ -181,7 +181,7 @@ pub const Package = struct {
 };
 
 fn checkMinZig(current: std.SemanticVersion, exe: *std.Build.Step.Compile) void {
-    const min = std.SemanticVersion.parse("0.14.0") catch return;
+    const min = std.SemanticVersion.parse("0.14.1") catch return;
     if (current.order(min).compare(.lt)) @panic(exe.step.owner.fmt("Your Zig version v{} does not meet the minimum build requirement of v{}", .{current, min}));
 }
 

zig.mod
@@ -3,7 +3,7 @@ name: zigmod
 main: src/lib.zig
 license: MIT
 description: A package manager for the Zig programming language.
-min_zig_version: 0.14.0
+min_zig_version: 0.14.1
 min_zigmod_version: r96
 dependencies:
   - src: git https://github.com/nektro/zig-yaml